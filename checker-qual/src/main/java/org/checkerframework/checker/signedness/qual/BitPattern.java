90d5f9668d75298f0057fb173030559316621cb4
KatherinePalevich <katherine.palevich@gmail.com>
2025-11-04 12:56:32 -0800

    removed subtype

--- checker-qual/src/main/java/org/checkerframework/checker/signedness/qual/BitPattern.java
+++ checker-qual/src/main/java/org/checkerframework/checker/signedness/qual/BitPattern.java
@@ -21,5 +21,5 @@ import org.checkerframework.framework.qual.SubtypeOf;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
-@SubtypeOf({UnknownSignedness.class, SignednessBottom.class})
+@SubtypeOf({UnknownSignedness.class})
 public @interface BitPattern {}
