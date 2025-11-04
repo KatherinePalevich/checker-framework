09e18132a4b2558b5b7611ee9d4d7898784052d2
KatherinePalevich <katherine.palevich@gmail.com>
2025-11-04 12:33:06 -0800

    fixed gradle error

--- checker-qual/src/main/java/org/checkerframework/checker/signedness/qual/BitPattern.java
+++ checker-qual/src/main/java/org/checkerframework/checker/signedness/qual/BitPattern.java
@@ -6,7 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.SubtypeOf;
-import org.checkerframework.framework.qual.SupertypeOf;
 
 /**
  * A type qualifier for values that are not arithmetic quantities, but are manipulated as a pattern
